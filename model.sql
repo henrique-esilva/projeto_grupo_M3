ff88aa3a7c6e790596db2a859f86a7d36839e79e
Luana Duarte <143774640+Luarduarte@users.noreply.github.com>
2023-11-17 16:49:29 -0300

    Update model.sql

--- model.sql
+++ model.sql
@@ -14,6 +14,23 @@ telefone VARCHAR(17) NOT NULL,
 escolaridade VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE curso(
+id_curso INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nome_curso VARCHAR (20) NOT NULL,
+id_disciplina INT (10) NOT NULL,
+carga_horaria VARCHAR (10) NOT NULL,
+turno VARCHAR (20) NOT NULL,
+data_inicio DATE NOT NULL,
+data_fim DATE NOT NULL
+);
+
+CREATE TABLE modulo(
+id_modulo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR (20) NOT NULL,
+carga_horaria INT NOT NULL,
+id_curso INT NOT NULL 
+);
+
 CREATE TABLE facilitadores(
 id_facilitador INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100) NOT NULL,
@@ -22,26 +39,33 @@ endereco VARCHAR(150) NOT NULL,
 nascimento DATE NOT NULL,
 email VARCHAR(100) NOT NULL,
 telefone VARCHAR(17) NOT NULL,
-especialidade VARCHAR(20)
+id_disciplina INT NOT NULL
 );
 
 CREATE TABLE turmas(
 id_turma INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id_curso INT NOT NULL,
+id_aluno INT NOT NULL,
 sala VARCHAR(10) NOT NULL,
-especialidade VARCHAR(20) NOT NULL,
 nivel VARCHAR(20) NOT NULL,
-inicio DATE NOT NULL,
-final DATE NOT NULL,
+data_inicio DATE NOT NULL,
+data_fim DATE NOT NULL,
+horario_inicio TIME NOT NULL,
+horario_fim TIME NOT NULL,
 periodo VARCHAR(12) NOT NULL,
-id_facilitador INT
+dia VARCHAR (20) NOT NULL,
+id_turno INT NOT NULL
 );
-ALTER TABLE turmas ADD CONSTRAINT fk_facilitador
-FOREIGN KEY (id_facilitador) REFERENCES facilitadores (id_facilidador);
 
-CREATE TABLE alunos_turmas(
-id_aluno INT,
-id_turma INT,
-PRIMARY KEY (id_aluno, id_turma),
-FOREIGN KEY (id_aluno) REFERENCES alunos(id_aluno),
-FOREIGN KEY (id_turma) REFERENCES turmas(id_turma)
+CREATE TABLE disciplina(
+id_disciplina INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id_modulo INT NOT NULL,
+nome VARCHAR(30) NOT NULL
+
 );
+
+CREATE TABLE turno(
+id_turno INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+periodo VARCHAR(12) NOT NULL
+);
+
