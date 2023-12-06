fe3cc8e96770eff4953f17b186df234e6ccb2179
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-06 13:49:12 -0300

    Update curso.sql

--- inserts/curso.sql
+++ inserts/curso.sql
@@ -1,34 +1,19 @@
 INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('1','Análise de Dados','1', '360','2024/01/07','2024/08/07');
- 
- INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('2','Excel','2', '360','2024/01/07','2024/08/07');
- 
- INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('3','Power BI','3', '360','2024/01/07','2024/08/07');
- 
- INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('4','Análise e desenvolvimento de sistemas','4', '360','2024/01/07','2024/08/07');
- 
- INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('5','Banco de Dados(SQL)','5', '360','2024/02/08','2024/09/08');
- 
- INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('6','Desenvolvedor Beck-end','6', '360','2024/02/08','2024/09/08');
- 
- INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('7','Office 365','7', '360','2024/02/08','2024/09/08');
- 
- INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
- VALUES
- ('8','Python','8', '360','2024/01/07','2024/08/07');
+VALUES ('1','Análise de Dados','1', '360','2024/01/07','2024/08/07'); 
+INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
+VALUES ('2','Excel','2', '360','2024/01/07','2024/08/07');
+INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
+VALUES ('3','Power BI','3', '360','2024/01/07','2024/08/07');
+INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
+VALUES ('4','Análise e desenvolvimento de sistemas','4', '360','2024/01/07','2024/08/07');
+INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
+VALUES ('5','Banco de Dados(SQL)','5', '360','2024/02/08','2024/09/08');
+INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
+VALUES ('6','Desenvolvedor Beck-end','6', '360','2024/02/08','2024/09/08');
+INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
+VALUES ('7','Office 365','7', '360','2024/02/08','2024/09/08');
+INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim)
+VALUES ('8','Python','8', '360','2024/01/07','2024/08/07');
  
  
  
