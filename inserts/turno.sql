6357c68e82a4ae9483bb89fe5e39ed2ecd540f8d
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-12-05 23:14:00 -0300

    Update turno.sql

--- inserts/turno.sql
+++ inserts/turno.sql
@@ -1,1 +1,5 @@
+INSERT INTO turno(id_turno, periodo, hora_inicio, hora_termino) VALUES ('1', 'manhã','08:00','12:00');
 
+INSERT INTO turno(id_turno, periodo, hora_inicio, hora_termino) VALUES ('2', 'tarde','13:00','17:00');
+
+INSERT INTO turno(id_turno, periodo, hora_inicio, hora_termino) VALUES ('3', 'noite','18:00','22:00');
