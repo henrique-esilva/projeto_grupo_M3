f30092d72bb3f134c18aab07cdd86de15f879b01
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-07 09:39:07 -0300

    Update curso.sql

--- inserts/curso.sql
+++ inserts/curso.sql
@@ -1,9 +1,9 @@
 INSERT INTO curso(id_curso, nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim) VALUES
-(NULL,'Análise de Dados',1, '360','2024-01-07','2024-08-07'),
-(NULL,'Excel','2', '360','2024-01-07','2024-08-07'),
-(NULL,'Power BI','3', '360','2024-01-07','2024-08-07'),
-(NULL,'Análise e desenvolvimento de sistemas','4', '360','2024-01-07','2024-08-07'),
-(NULL,'Banco de Dados(SQL)','5', '360','2024-02-08','2024-09-08'),
-(NULL,'Desenvolvedor Beck-end','6', '360','2024-02-08','2024-09-08'),
-(NULL,'Office 365','7', '360','2024-02-08','2024-09-08'),
-(NULL,'Python','8', '360','2024-01-07','2024-08-07');
+('Análise de Dados',1, '360','2024-01-07','2024-08-07'),
+('Excel','2', '360','2024-01-07','2024-08-07'),
+('Power BI','3', '360','2024-01-07','2024-08-07'),
+('Análise e desenvolvimento de sistemas','4', '360','2024-01-07','2024-08-07'),
+('Banco de Dados(SQL)','5', '360','2024-02-08','2024-09-08'),
+('Desenvolvedor Beck-end','6', '360','2024-02-08','2024-09-08'),
+('Office 365','7', '360','2024-02-08','2024-09-08'),
+('Python','8', '360','2024-01-07','2024-08-07');
