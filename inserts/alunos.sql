388ad552bfc7ffc73e8888c3809d4eede442367f
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-12-08 00:17:27 -0300

    Update alunos.sql
    adição de mais inserts para a tabela alunos

--- inserts/alunos.sql
+++ inserts/alunos.sql
@@ -29,3 +29,117 @@ INSERT INTO alunos (id_aluno, matricula, nome, nascimento, genero, cpf, endereco
 (NULL, "28", "Miguel Antunes",    "2003-01-16", "masculino", "10000000028", "Cinelândia, Rio de Janeiro - RJ",  "miguelantunes@yahoo.com",       "(14)91234-5678", 'ensino médio incompleto', ),
 (NULL, "29", "Rafaela Silva",     "2000-07-01", "feminino",  "10000000029", "Paraíso, São Gonçalo - RJ",        "rafaelasilva@gmail.com",        "(24)98765-4321", 'ensino médio incompleto', ),
 (NULL, "30", "Andrei Mendes",     "1998-12-10", "masculino", "10000000030", "Baldeador, São Gonçalo - RJ",      "mendesandrei@hotmail.com",      "(34)98900-2345", 'ensino médio completo'  , );
+
+
+INSERT INTO alunos ( matricula, nome, nascimento, genero, cpf, endereco, email, telefone, escolaridade )
+VALUES
+( '0001', 'Ana Carolina', '1970-01-21', 'Feminino', '123.456.789-05', 'Rua São Cristóvão 123', 'anacarolina2@gmail.com', '998-579-247', 'Ensino Médio incompleto'),
+( '0002', 'Ester  Fonseca', '1995-09-27', 'Feminino', '160.415.395-50', 'Rua Xávier da Fonseca 756', 'ester@gmail.com', '999-654-321', 'Graduação');
+( '0003', 'Carlos Antônio', '1990-08-10', 'Masculino', '250.354.457-01', 'Av. Luis Ayrão 789', 'carlosantonio@gmail.com', '997-859-789', 'Pós-Graduação','1');
+( '0004', 'Maria Fernanda', '1997-08-20', 'Feminino', '377.444.555-30', 'Rua nove 1900', 'mariaf17@gmail.com', '979-631-894', 'Ensino Fundamental','1');
+( '0005', 'Roberto Campos', '2003-12-25', 'Masculino', '920.477.679-77', 'Av heitor Buarque 122', 'roberto885@gmail.com', '982-677-771', 'Cursando Ensino Médio','1');
+( '0006', 'Maria Gomes', '1997-07-22', 'Feminino', '231.888.265-88', 'Estrada do Aço 10', 'mariao2@gmail.com', '987-654-888', 'Cursando Curso técnico','1');
+( '0007', 'José  da Silva', '1999-09-28', 'Masculino', '131.248.182-29', 'Rua Mexico 28', 'jose8@gmail.com', '961-685-112', 'Superior completo','1');
+( '0008', 'Amanda Bittencourt', '2000-06-22', 'Feminino', '799.987.897-87', 'Rua Montana  171', 'amanda99@gmail.com', '987-654-820', 'Superior completo','1');
+( '0009', 'Luis Lima', '1990-12-08', 'Masculino', '322.133.132-21', 'Rua treze 22', 'ricardo00@gmail.com', '985-346-769', 'Ensino Médio Completo','1');
+( '0010', 'Luis Henrique', '1996-08-30', 'Masculino', '369.933.639-99', 'Rua Nova York 11', 'henrique001@gmail.com', '997-785-367', 'Ensino Médio Completo','1');
+( '0011', 'Lucas Moura', '1997-10-28', 'Masculino', '741.274.591-74', 'Av Ramos 40', 'lucas0202@gmail.com', '993-976-912', 'Ensino Médio Completo','1');
+( '0012', 'Camila Pitanga', '1989-08-20', 'Feminino', '276.759.427-99', 'Rua Meire Emanuela 33', 'camilaLima7@gmail.com', '990-000-899', 'Doutorado','2');
+( '0013', 'Fernando Costa ', '1992-09-25', 'Masculino', '724.111.054-08', 'Av Rei Pelé 55', 'fernandodark@gmail.com', '979-748-999', 'Pós-Graduação','2');
+( '0014', 'Cristiane Silva', '1988-05-12', 'Feminino', '748.965.825-77', 'Rua Antonio Castro 31', 'cristiane779@gmail.com', '968-655-976', 'Graduação','2');
+( '0015', 'Carlos Emanuel', '2003-08-18', 'Masculino', '788.364.202-66', 'Av. Francisco de Sá 66', 'carlos472@gmail.com', '999-653-789', 'Pós-Graduação','2');
+( '0016', 'Julianna Santos', '1960-02-26', 'Feminino', '251.796.249-99', 'Rua Egito 789', 'julianna789@gmail.com', '991-166-321', 'Ensino Médio completo','2');
+( '0017', 'Umberto Rocha', '1975-12-01', 'Masculino', '274.236.500-77', 'Av. Costa x', 'umberto28@gmail.com', '978-871-564', 'Ensino Médio completo','2');
+( '0018', 'Cirlene Matias', '1989-09-30', 'Feminino', '224.257.636-88', 'Rua das Flores 33', 'cirlene@gmail.com','982-021-664', 'Doutorado','2');
+('0019', 'Angela Figueiredo','2002-10-11', 'Feminino', '010.100.332-54', 'Rua das Magnolias 2', 'angelaf11@gmail.com', '888-001-002', 'Pós-Graduação','2');
+('0020', 'Isadora Fernandes','1987-01-01','Feminino', '020.212.236-61','Rua Jacatirão 22',' isa76@yahoo.com', '970-398-121', 'Ensino Médio Incompleto','2');
+ ('0021','Andressa Gusmão','2004-08-22','Feminino','011.222.333-44','Rua doze 1800','andressagusmao@yahoo.com','977-789-123','Graduação','2');
+ ('0022', 'Ana Louisa', '1973-06-11', 'Feminino', '678.456.789-05', 'Rua São Luis 123', 'analouisa2@gmail.com', '861-579-247', 'Ensino Médio completo', '3');
+( '0023', 'Helena Fonseca', '1977-01-13', 'Feminino', '123.456.789-77', 'Rua das Palmeiras 1', 'helenaf2@gmail.com', '781-579-247', 'Pos-graduação','3');
+('0024','André Bastos','1999-09-27','Masculino','120.545.891-01','Rua Seajesp 32','andrebastos@gmail.com','998-234-876','Graduação, cursando','3');
+('0025','Maria Helena','2010-10-10','Feminino','123.876.999-50','Rua Goias 100','maria@yahoo.com','999-767-234','Ensino médio incompleto','3');
+('0026','Andre Luis','1980-11-21','Masculino','456.786.999-48','Rua das acacias 108','andrel@yahoo.com','891-675-841','Pos-graduação, cursando','3');
+
+('0108', 'Pedro Henrique Silva', '1997-05-25', 'masculino', '10000000052', 'Botafogo, Rio de Janeiro - RJ', 'pedrosilva@gmail.com', '77912345678', 'graduação completa', '8'),
+('0109', 'Carolina Oliveira', '2000-08-15', 'feminino', '10000000053', 'Ipanema, Rio de Janeiro - RJ', 'carolinaoliveira@hotmail.com', '88987654321', 'ensino médio completo', '8'),
+('0110', 'Rafaela Santos', '1999-09-28', 'feminino', '10000000054', 'Tijuca, Rio de Janeiro - RJ', 'rafaelasantos@yahoo.com', '99912345678', 'graduação incompleta', '8'),
+('0111', 'João Pereira', '2001-03-21', 'masculino', '10000000055', 'Maracanã, Rio de Janeiro - RJ', 'joaopereira@gmail.com', '11987654321', 'ensino médio completo', '8'),
+('0112', 'Julia Ferreira', '1998-11-12', 'feminino', '10000000056', 'Copacabana, Rio de Janeiro - RJ', 'juliaferreira@hotmail.com', '22912345678', 'ensino médio incompleto', '8'),
+('0113', 'Lucas Oliveira', '2000-04-15', 'masculino', '10000000057', 'Barra da Tijuca, Rio de Janeiro - RJ', 'lucasoliveira@gmail.com', '33987654321', 'graduação completa', '8'),
+('0114', 'Gabriela Lima', '2003-01-29', 'feminino', '10000000058', 'Leblon, Rio de Janeiro - RJ', 'gabrielalima@yahoo.com', '44912345678', 'graduação completa', '8'),
+('0115', 'Matheus Santos', '1999-06-18', 'masculino', '10000000059', 'Laranjeiras, Rio de Janeiro - RJ', 'matheussantos@gmail.com', '55987654321', 'ensino médio incompleto', '8'),
+('0116', 'Ana Carolina Alves', '2002-02-20', 'feminino', '10000000060', 'Grajaú, Rio de Janeiro - RJ', 'anacarolinaalves@hotmail.com', '66912345678', 'graduação completa', '8'),
+('0117', 'Gustavo Pereira', '1998-04-03', 'masculino', '10000000061', 'Botafogo, Rio de Janeiro - RJ', 'gustavopereira@gmail.com', '77987654321', 'graduação completa', '8'),
+('0118', 'Mariana Costa', '2000-07-25', 'feminino', '10000000062', 'Ipanema, Rio de Janeiro - RJ', 'marianacosta@yahoo.com', '88912345678', 'ensino médio completo', '8'),
+('0119', 'Rodrigo Oliveira', '1999-08-18', 'masculino', '10000000063', 'Tijuca, Rio de Janeiro - RJ', 'rodrigooliveira@gmail.com', '99987654321', 'ensino médio completo', '8'),
+('0120', 'Carolina Lima', '2001-11-21', 'feminino', '10000000064', 'Maracanã, Rio de Janeiro - RJ', 'carolinalima@hotmail.com', '11912345678', 'graduação incompleta', '8'),
+('0121', 'João Santos', '1998-12-12', 'masculino', '10000000065', 'Copacabana, Rio de Janeiro - RJ', 'joaosantos@yahoo.com', '22987654321', 'ensino médio completo', '8'),
+('0122', 'Maria Fernanda Oliveira', '2000-03-15', 'feminino', '10000000066', 'Barra da Tijuca, Rio de Janeiro - RJ', 'mariafernandaoliveira@gmail.com', '33912345678', 'ensino médio incompleto', '8'),
+('0123', 'Pedro Henrique Pereira', '1999-06-28', 'masculino', '10000000067', 'Leblon, Rio de Janeiro - RJ', 'pedrohpereira@hotmail.com', '44987654321', 'graduação completa', '8'),
+('0124', 'Gabriela Santos', '2003-01-29', 'feminino', '10000000068', 'Laranjeiras, Rio de Janeiro - RJ', 'gabrielasantos@gmail.com', '55912345678', 'ensino médio completo', '8'),
+('0125', 'Matheus Oliveira', '1999-06-18', 'masculino', '10000000069', 'Grajaú, Rio de Janeiro - RJ', 'matheusoliveira@yahoo.com', '66987654321', 'ensino médio incompleto', '8'),
+('0126', 'Ana Carolina Costa', '2002-02-20', 'feminino', '10000000070', 'Botafogo, Rio de Janeiro - RJ', 'anacarolinacosta@gmail.com', '77912345678', 'graduação completa', '8'),
+('0127', 'Gustavo Lima', '1998-04-03', 'masculino', '10000000071', 'Ipanema, Rio de Janeiro - RJ', 'gustavolima@hotmail.com', '88987654321', 'graduação completa', '8'),
+('0128', 'Mariana Santos', '2000-07-25', 'feminino', '10000000072', 'Tijuca, Rio de Janeiro - RJ', 'marianasantos@gmail.com', '99912345678', 'ensino médio completo', '8');
+ ('0129', 'Rodrigo Oliveira', '1999-08-18', 'masculino', '10000000073', 'Copacabana, Rio de Janeiro - RJ', 'rodrigooliveira@gmail.com', '11987654321', 'graduação incompleta', '5'),
+('0130', 'Carolina Lima', '2001-11-21', 'feminino', '10000000074', 'Maracanã, Rio de Janeiro - RJ', 'carolinalima@hotmail.com', '22912345678', 'ensino médio completo', '5'),
+('0131', 'João Santos', '1998-12-12', 'masculino', '10000000075', 'Botafogo, Rio de Janeiro - RJ', 'joaosantos@yahoo.com', '33987654321', 'ensino médio completo', '5'),
+('0132', 'Maria Fernanda Oliveira', '2000-03-15', 'feminino', '10000000076', 'Ipanema, Rio de Janeiro - RJ', 'mariafernandaoliveira@gmail.com', '44912345678', 'graduação completa', '6'),
+('0133', 'Pedro Henrique Pereira', '1999-06-28', 'masculino', '10000000077', 'Tijuca, Rio de Janeiro - RJ', 'pedrohpereira@hotmail.com', '55987654321', 'ensino médio incompleto', '6'),
+('0134', 'Gabriela Santos', '2003-01-29', 'feminino', '10000000078', 'Leblon, Rio de Janeiro - RJ', 'gabrielasantos@gmail.com', '66912345678', 'graduação completa', '6'),
+('0135', 'Matheus Oliveira', '1999-06-18', 'masculino', '10000000079', 'Laranjeiras, Rio de Janeiro - RJ', 'matheusoliveira@yahoo.com', '77987654321', 'ensino médio incompleto', '6'),
+('0136', 'Ana Carolina Costa', '2002-02-20', 'feminino', '10000000080', 'Grajaú, Rio de Janeiro - RJ', 'anacarolinacosta@gmail.com', '88912345678', 'graduação completa', '6'),
+('0137', 'Gustavo Lima', '1998-04-03', 'masculino', '10000000081', 'Botafogo, Rio de Janeiro - RJ', 'gustavolima@hotmail.com', '99987654321', 'graduação completa', '6'),
+('0138', 'Mariana Santos', '2000-07-25', 'feminino', '10000000082', 'Ipanema, Rio de Janeiro - RJ', 'marianasantos@gmail.com', '11912345678', 'ensino médio completo', '6'),
+('0139', 'Rodrigo Oliveira', '1999-08-18', 'masculino', '10000000083', 'Copacabana, Rio de Janeiro - RJ', 'rodrigooliveira@gmail.com', '22987654321', 'graduação incompleta', '6'),
+('0140', 'Carolina Lima', '2001-11-21', 'feminino', '10000000084', 'Maracanã, Rio de Janeiro - RJ', 'carolinalima@hotmail.com', '33912345678', 'ensino médio completo', '6'),
+('0141', 'João Santos', '1998-12-12', 'masculino', '10000000085', 'Botafogo, Rio de Janeiro - RJ', 'joaosantos@yahoo.com', '44987654321', 'ensino médio completo', '6'),
+('0142', 'Maria Fernanda Oliveira', '2000-03-15', 'feminino', '10000000086', 'Ipanema, Rio de Janeiro - RJ', 'mariafernandaoliveira@gmail.com', '55912345678', 'graduação completa', '7'),
+('0143', 'Pedro Henrique Pereira', '1999-06-28', 'masculino', '10000000087', 'Tijuca, Rio de Janeiro - RJ', 'pedrohpereira@hotmail.com', '66987654321', 'ensino médio incompleto', '7'),
+('0144', 'Gabriela Santos', '2003-01-29', 'feminino', '10000000088', 'Leblon, Rio de Janeiro - RJ', 'gabrielasantos@gmail.com', '77912345678', 'graduação completa', '7'),
+('0145', 'Matheus Oliveira', '1999-06-18', 'masculino', '10000000089', 'Laranjeiras, Rio de Janeiro - RJ', 'matheusoliveira@yahoo.com', '88987654321', 'ensino médio incompleto', '7'),
+('0146', 'Ana Carolina Costa', '2002-02-20', 'feminino', '10000000090', 'Grajaú, Rio de Janeiro - RJ', 'anacarolinacosta@gmail.com', '99912345678', 'graduação completa', '7'),
+('0147', 'Gustavo Lima', '1998-04-03', 'masculino', '10000000091', 'Botafogo, Rio de Janeiro - RJ', 'gustavolima@hotmail.com', '11987654321', 'graduação completa', '7'),
+('0148', 'Mariana Santos', '2000-07-25', 'feminino', '10000000092', 'Ipanema, Rio de Janeiro - RJ', 'marianasantos@gmail.com', '22912345678', 'ensino médio completo', '7'),
+('0149', 'Rodrigo Oliveira', '1999-08-18', 'masculino', '10000000093', 'Copacabana, Rio de Janeiro - RJ', 'rodrigooliveira@gmail.com', '33987654321', 'graduação incompleta', '7'),
+('0150', 'Carolina Lima', '2001-11-21', 'feminino', '10000000094', 'Maracanã, Rio de Janeiro - RJ', 'carolinalima@hotmail.com', '44912345678', 'ensino médio completo', '7'),
+('0151', 'João Santos', '1998-12-12', 'masculino', '10000000095', 'Botafogo, Rio de Janeiro - RJ', 'joaosantos@yahoo.com', '55987654321', 'ensino médio completo', '7'),
+('0152', 'Maria Fernanda Oliveira', '2000-03-15', 'feminino', '10000000096', 'Ipanema, Rio de Janeiro - RJ', 'mariafernandaoliveira@gmail.com', '66912345678', 'graduação completa', '8'),
+('0153', 'Pedro Henrique Pereira', '1999-06-28', 'masculino', '10000000097', 'Tijuca, Rio de Janeiro - RJ', 'pedrohpereira@hotmail.com', '77987654321', 'ensino médio incompleto', '8'),
+('0154', 'Gabriela Santos', '2003-01-29', 'feminino', '10000000098', 'Leblon, Rio de Janeiro - RJ', 'gabrielasantos@gmail.com', '88912345678', 'graduação completa', '8'),
+('0155', 'Matheus Oliveira', '1999-06-18', 'masculino', '10000000099', 'Laranjeiras, Rio de Janeiro - RJ', 'matheusoliveira@yahoo.com', '99987654321', 'ensino médio incompleto', '8'),
+('0156', 'Ana Carolina Costa', '2002-02-20', 'feminino', '10000000100', 'Grajaú, Rio de Janeiro - RJ', 'anacarolinacosta@gmail.com', '11912345678', 'graduação completa', '8'),
+('0157', 'Gustavo Lima', '1998-04-03', 'masculino', '10000000101', 'Botafogo, Rio de Janeiro - RJ', 'gustavolima@hotmail.com', '22987654321', 'graduação completa', '8'),
+('0158', 'Mariana Santos', '2000-07-25', 'feminino', '10000000102', 'Ipanema, Rio de Janeiro - RJ', 'marianasantos@gmail.com', '33912345678', 'ensino médio completo', '8'),
+('0159', 'Rodrigo Oliveira', '1999-08-18', 'masculino', '10000000103', 'Copacabana, Rio de Janeiro - RJ', 'rodrigooliveira@gmail.com', '44987654321', 'graduação incompleta', '8'),
+('0160', 'Carolina Lima', '2001-11-21', 'feminino', '10000000104', 'Maracanã, Rio de Janeiro - RJ', 'carolinalima@hotmail.com', '55912345678', 'ensino médio completo', '8');
+('0182', 'Gabriel Oliveira', '1998-05-12', 'Masculino', '111.222.333-40', 'Rua dos Pinheiros, 123', 'gabriel.oliveira@example.com', '123-456-789', 'Graduação', '7'),
+('0183', 'Sophia Almeida', '1999-02-28', 'Feminino', '444.555.666-41', 'Avenida Central, 567', 'sophia.almeida@example.com', '987-654-321', 'Ensino Médio', '7'),
+('0184', 'Lucas Santos', '2001-11-15', 'Masculino', '777.888.999-42', 'Rua das Flores, 890', 'lucas.santos@example.com', '234-567-890', 'Graduação', '7'),
+('0185', 'Julia Costa', '1997-08-03', 'Feminino', '333.444.555-43', 'Alameda das Águias, 456', 'julia.costa@example.com', '876-543-210', 'Pós-Graduação', '7'),
+('0186', 'Pedro Lima', '2000-04-17', 'Masculino', '666.777.888-44', 'Travessa dos Girassóis, 789', 'pedro.lima@example.com', '543-210-987', 'Ensino Fundamental', '7'),
+('0187', 'Lara Pereira', '1998-09-25', 'Feminino', '999.000.111-45', 'Rua das Oliveiras, 345', 'lara.pereira@example.com', '654-321-098', 'Graduação', '7'),
+('0188', 'Mateus Silva', '1995-06-21', 'Masculino', '222.333.444-46', 'Avenida da Paz, 678', 'mateus.silva@example.com', '789-012-345', 'Mestrado', '7'),
+('0189', 'Camila Vieira', '1996-12-07', 'Feminino', '555.666.777-47', 'Rua dos Lírios, 901', 'camila.vieira@example.com', '210-987-654', 'Ensino Médio', '7'),
+('0190', 'Enzo Martins', '2002-07-14', 'Masculino', '888.999.000-48', 'Alameda das Hortênsias, 234', 'enzo.martins@example.com', '321-098-765', 'Graduação', '7'),
+('0191', 'Isabella Ferreira', '1994-10-19', 'Feminino', '111.222.333-49', 'Rua das Violetas, 567', 'isabella.ferreira@example.com', '432-109-876', 'Graduação', '7'),
+('0192', 'Daniel Oliveira', '1999-03-01', 'Masculino', '444.555.666-50', 'Travessa dos Cravos, 890', 'daniel.oliveira@example.com', '543-210-987', 'Ensino Médio', '7'),
+('0193', 'Manuela Costa', '2001-05-28', 'Feminino', '777.888.999-51', 'Avenida das Rosas, 123', 'manuela.costa@example.com', '654-321-098', 'Graduação', '7'),
+('0194', 'Vitor Almeida', '1997-12-15', 'Masculino', '333.444.555-52', 'Rua dos Jasmins, 456', 'vitor.almeida@example.com', '765-432-109', 'Pós-Graduação', '7'),
+('0195', 'Laura Santos', '2000-02-09', 'Feminino', '666.777.888-53', 'Alameda dos Crisântemos, 789', 'laura.santos@example.com', '876-543-210', 'Ensino Fundamental', '7'),
+('0196', 'Arthur Lima', '1998-07-25', 'Masculino', '999.000.111-54', 'Rua das Margaridas, 345', 'arthur.lima@example.com', '987-654-321', 'Graduação', '7'),
+('0197', 'Helena Pereira', '1995-04-01', 'Feminino', '222.333.444-55', 'Avenida das Orquídeas, 678', 'helena.pereira@example.com', '098-765-432', 'Mestrado', '7'),
+('0198', 'Gabriel Silva', '1996-11-07', 'Masculino', '555.666.777-56', 'Travessa das Begônias, 901', 'gabriel.silva@example.com', '109-876-543', 'Ensino Médio', '7'),
+('0199', 'Sophia Vieira', '2002-08-14', 'Feminino', '888.999.000-57', 'Alameda dos Lírios, 234', 'sophia.vieira@example.com', '210-987-654', 'Graduação', '7'),
+('0200', 'Lucas Martins', '1994-01-19', 'Masculino', '111.222.333-58', 'Rua dos Jasmins, 567', 'lucas.martins@example.com', '321-098-765', 'Graduação', '7'),
+('0201', 'Julia Lima', '1999-03-23', 'Feminino', '444.555.666-59', 'Travessa das Orquídeas, 890', 'julia.lima@example.com', '432-109-876', 'Ensino Médio', '7'),
+('0202', 'Pedro Oliveira', '2001-06-30', 'Masculino', '777.888.999-60', 'Alameda das Margaridas, 123', 'pedro.oliveira@example.com', '543-210-987', 'Graduação', '7'),
+('0203', 'Lara Costa', '1997-10-05', 'Feminino', '333.444.555-61', 'Rua dos Crisântemos, 456', 'lara.costa@example.com', '654-321-098', 'Pós-Graduação', '7'),
+('0204', 'Mateus Almeida', '2000-01-10', 'Masculino', '666.777.888-62', 'Avenida das Begônias, 789', 'mateus.almeida@example.com', '765-432-109', 'Ensino Fundamental', '7'),
+('0205', 'Camila Santos', '1998-06-16', 'Feminino', '999.000.111-63', 'Travessa das Violetas, 123', 'camila.santos@example.com', '876-543-210', 'Graduação', '7'),
+('0206', 'Enzo Pereira', '1995-09-21', 'Masculino', '222.333.444-64', 'Alameda das Rosas, 456', 'enzo.pereira@example.com', '987-654-321', 'Mestrado', '7'),
+('0207', 'Isabella Silva', '2001-02-26', 'Feminino', '555.666.777-65', 'Rua das Margaridas, 789', 'isabella.silva@example.com', '098-765-432', 'Ensino Médio', '7'),
+('0208', 'Daniel Lima', '1997-11-01', 'Masculino', '888.999.000-66', 'Avenida dos Crisântemos, 901', 'daniel.lima@example.com', '109-876-543', 'Graduação', '7'),
+('0209', 'Manuela Oliveira', '2000-04-08', 'Feminino', '111.222.333-67', 'Travessa das Orquídeas, 234', 'manuela.oliveira@example.com', '210-987-654', 'Pós-Graduação', '7'),
+('0210', 'Vitor Costa', '1999-05-13', 'Masculino', '444.555.666-68', 'Alameda das Lírios, 567', 'vitor.costa@example.com', '321-098-765', 'Graduação', '7'),
+('0211', 'Laura Vieira', '1996-08-18', 'Feminino', '777.888.999-69', 'Rua dos Jasmins, 890', 'laura.vieira@example.com', '432-109-876', 'Graduação', '7');
