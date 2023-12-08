0c7daf470279cd40b52b081bb53a61f5431f0009
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-12-07 23:06:05 -0300

    Update facilitadores.sql

--- inserts/facilitadores.sql
+++ inserts/facilitadores.sql
@@ -1,1 +1,8 @@
+INSERT INTO facilitadores (id_facilitador, nome, cpf, endereco, nascimento, email, telefone) 
+VALUES 
+('1', 'Alberto', '000.000.000-00', 'Rua 7, Calaboca, Santa Paula - RJ', date('1990-03-20'), 'albertorm@outlook.com', '+55(21)90000-0001'), 
+('2', 'Letícia', '000.000.000-01', 'Rua Americo Oberlander, Santa Rosa, Niterói - RJ', date('1998-11-02'), 'leticiaad@gmail.com', '+55(21)90000-0002'), 
+('3', 'Allanis', '000.000.000-03', 'Calaboca, Santa Paula - RJ', date('1990-09-10'), 'allanismoura@hotmail.com', '+55(21)90000-0004'), 
+('4', 'Jonas', '000.000.000-04', 'Rua Iracema de Alencar, Maria Paula, São Gonçalo - RJ', date('1990-03-20'), 'jonaslessa@outlook.com', '+55(21)90000-0005');
+('5','Marina','000.000.000-05','Avenida Brasil, Centro, Rio de Janeiro - RJ','1995-08-15','marinagomes@email.com','+55(21)90000-0006');
 
