93e45c8e39ba493f6170f3c4e4f6df9cd12aa1e3
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-09 14:45:57 -0300

    Update model.sql

--- model.sql
+++ model.sql
@@ -22,18 +22,14 @@ id_turma INT
 CREATE TABLE curso(
 id_curso INT PRIMARY KEY AUTO_INCREMENT,
 nome_curso VARCHAR (64) NOT NULL,
-id_disciplina INT (10) NOT NULL,
-carga_horaria VARCHAR (10) NOT NULL,
-data_inicio DATE NOT NULL,
-data_fim DATE NOT NULL
+carga_horaria VARCHAR (10) NOT NULL
 );
 
 CREATE TABLE modulo(
 id_modulo INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR (20) NOT NULL,
 carga_horaria INT NOT NULL,
-id_curso INT NOT NULL,
-id_turma INT NOT NULL
+id_curso INT NOT NULL
 );
 
 CREATE TABLE facilitadores(
@@ -43,14 +39,12 @@ cpf VARCHAR(14) NOT NULL,
 endereco VARCHAR(150) NOT NULL,
 nascimento DATE NOT NULL,
 email VARCHAR(100) NOT NULL,
-telefone VARCHAR(17) NOT NULL,
-id_disciplina INT NOT NULL
+telefone VARCHAR(17) NOT NULL
 );
 
 CREATE TABLE turmas(
 id_turma INT PRIMARY KEY AUTO_INCREMENT,
 id_curso INT NOT NULL,
-id_aluno INT NOT NULL,
 sala VARCHAR(10) NOT NULL,
 nivel VARCHAR(20) NOT NULL,
 data_inicio DATE NOT NULL,
@@ -85,22 +79,24 @@ nome VARCHAR(30) NOT NULL
 
 CREATE TABLE turno(
 id_turno INT PRIMARY KEY AUTO_INCREMENT,
-periodo VARCHAR(12) NOT NULL
+periodo VARCHAR(12) NOT NULL,
+hora_inicio TIME,
+hora_termino TIME
 );
 
 
-ALTER TABLE modulo ADD CONSTRAINT fk_modulo_turmas FOREIGN KEY (id_turma) REFERENCES turmas(id_turma);
+-- ALTER TABLE modulo ADD CONSTRAINT fk_modulo_turmas FOREIGN KEY (id_turma) REFERENCES turmas(id_turma);
 ALTER TABLE alunos ADD CONSTRAINT fk_alunos_turmas FOREIGN KEY (id_turma) REFERENCES turmas(id_turma);
+
+
 ALTER TABLE disciplina ADD CONSTRAINT fk_disciplina_modulo FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo);
-
-
 ALTER TABLE turmas ADD CONSTRAINT fk_turmas_turno FOREIGN KEY (id_turno) REFERENCES turno(id_turno);
 ALTER TABLE turmas ADD CONSTRAINT fk_turmas_curso FOREIGN KEY (id_curso) REFERENCES curso(id_curso);
 ALTER TABLE disciplina ADD CONSTRAINT fk_disciplina_facilitadores FOREIGN KEY (id_facilitador) REFERENCES facilitadores(id_facilitador);
+ALTER TABLE modulo ADD CONSTRAINT fk_modulo_curso FOREIGN KEY (id_curso) REFERENCES curso(id_curso);
 
 /*
--- não remover o conteúdo deste comentário 
-ALTER TABLE modulo ADD CONSTRAINT fk_modulo_curso FOREIGN KEY (id_curso) REFERENCES curso(id_curso);
+-- relacionamento muitos para muitos - provável que não seja mais usado
 ALTER TABLE alunos_has_turmas
   ADD CONSTRAINT fk_alunos_has_turmas__alunos FOREIGN KEY (id_aluno) REFERENCES alunos (id_aluno),
   ADD CONSTRAINT fk_alunos_has_turmas__turmas FOREIGN KEY (id_turma) REFERENCES turmas (id_turma);
