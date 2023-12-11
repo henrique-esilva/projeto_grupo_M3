45111849943708ec92b8fdc7d71ef0c664ec5263
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-12-11 14:09:03 -0300

    Update curso.sql

--- inserts/curso.sql
+++ inserts/curso.sql
@@ -1,6 +1,5 @@
 INSERT INTO curso(nome_curso, id_disciplina, carga_horaria,  data_inicio, data_fim) VALUES
 ('Análise de Dados',1, '360','2024-01-07','2024-08-07'),
-('Excel','2', '360','2024-01-07','2024-08-07'),
 ('Power BI','3', '360','2024-01-07','2024-08-07'),
 ('Análise e desenvolvimento de sistemas','4', '360','2024-01-07','2024-08-07'),
 ('Banco de Dados(SQL)','5', '360','2024-02-08','2024-09-08'),
