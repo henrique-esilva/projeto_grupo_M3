f8ab766af1a832e3a326170789654fc07aaf24b8
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-11 02:47:56 -0300

    Update importCSV.sql

--- inserts/importCSV.sql
+++ inserts/importCSV.sql
@@ -19,8 +19,40 @@ LOAD DATA INFILE 'modulo.csv' INTO TABLE modulo
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
-#INSERT INTO modulo(nome, carga_horaria, id_curso) VALUES ('1', 180, 1), ('2', 180, 1), ('1', 180, 2), ('2', 180, 2), ('1', 180, 3), ('2', 180, 3), ('1', 180, 4), ('2', 180, 4), ('1', 180, 5), ('2', 180, 5), ('1', 180, 6), ('2', 180, 6), ('1', 180, 7), ('2', 180, 7);
+-- INSERT INTO modulo(nome, carga_horaria, id_curso) VALUES ('1', 180, 1), ('2', 180, 1), ('1', 180, 2), ('2', 180, 2), ('1', 180, 3), ('2', 180, 3), ('1', 180, 4), ('2', 180, 4), ('1', 180, 5), ('2', 180, 5), ('1', 180, 6), ('2', 180, 6), ('1', 180, 7), ('2', 180, 7);
 SELECT * FROM modulo;
+
+DELETE FROM disciplina WHERE id_disciplina>0;
+INSERT INTO disciplina(id_modulo, nome) VALUES
+(01, 'Fundamentos de dados'),
+(01, 'Estrutura de dados'),
+(02, 'Técnicas de análise'),
+(02, 'Ferramentas de BI'),
+(03, 'Básico do Power BI'),
+(03, 'Dashboards'),
+(04, 'Optimização do tempo'),
+(04, 'APIs e ferramentas'),
+(05, 'Arquiteturas de Software'),
+(05, 'Paradigmas e lógica de programação'),
+(06, 'DRY, MVC e outros Fundamentos'),
+(06, 'Sistemas operacionais'),
+(07, 'Fundamentos básicos'),
+(07, 'Modelagem'),
+(08, 'Funções e gatilhos'),
+(08, 'Sintaxe avançada'),
+(09, 'Introdução ao back-end e ao MVC'),
+(09, 'Desenvlvimento escalável'),
+(10, 'Programação em PHP'),
+(10, 'Programação em C#'),
+(11, 'Núvem e compartilhamento'),
+(11, 'Word'),
+(12, 'PowerPoint'),
+(12, 'Excel'),
+(13, 'Fundamentos da programação'),
+(13, 'Lógica de programação'),
+(14, 'Orientação a objetos'),
+(14, 'Modularização e bibliotecas');
+SELECT * FROM disciplina;
 
 DELETE FROM turno WHERE id_turno>0;
 INSERT INTO turno(id_turno, periodo, hora_inicio, hora_termino) VALUES
